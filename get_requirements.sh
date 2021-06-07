79a86bf54ea4787902c4007a2b4aa08e4bbf1b17
Ed Giese <edgiese@gmail.com>
2021-06-07 21:34:05 +0000

    Start moving to ansible

---
+++ get_requirements.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y python3-pip
+pip3 install psutil
+ansible-galaxy collection install community.general
+ansible-galaxy install moviedo.nvm
+ansible-galaxy install comcast.sdkman
